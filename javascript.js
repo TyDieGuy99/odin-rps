a60c95ca2014c1374c4410c02bd7fa3eaaabc256
TyDieGuy99 <120081348+TyDieGuy99@users.noreply.github.com>
2025-03-23 14:34:45 -0400

    Added player input Created function for player input of rps + some comments

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
 console.log("Hello World");
-const max = 3;
+
+const max = 3; //all 3 rps options
 
 console.log(getComputerChoice());
+getHumanChoice();
 
+//this converts random number 0-2 into rps option
 function getComputerChoice() {
 
     let x = Math.floor(Math.random() * max);
@@ -18,4 +21,18 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+function getHumanChoice() {
+    let playerChoice = prompt('Rock, Paper, Scissors', 'Choose an option..'); //ask for player input
+    //use .toLowerCase() to account for if player capitalizes any letter in their input
+    if (playerChoice.toLowerCase() === 'rock') {
+        return console.log('I Choose Rock')
+    } else if (playerChoice.toLowerCase() === 'paper') {
+        return console.log('I Choose Paper')
+    } else if (playerChoice.toLowerCase() === 'scissors') {
+        return console.log('I Choose Scissors')
+    } else {
+        return null; //if player doesn't type any of the 3 options
+    }
+        
+}
 
