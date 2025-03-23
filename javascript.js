13df83ec3d85ec7f6a484f9623f4f317685fbbef
TyDieGuy99 <120081348+TyDieGuy99@users.noreply.github.com>
2025-03-23 13:57:27 -0400

    Added function getComputerChoice randomly generates a rps option

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+const max = 3;
+
+console.log(getComputerChoice());
+
+function getComputerChoice() {
+
+    let x = Math.floor(Math.random() * max);
+    let computerChoice = 'none';
+
+    if (x == 0) { 
+        computerChoice = 'Rock';
+    } else if (x == 1) {
+        computerChoice = 'Paper';
+    } else { 
+        computerChoice= 'Scissors';
+    }
+    return console.log(computerChoice);
+}
+
+
