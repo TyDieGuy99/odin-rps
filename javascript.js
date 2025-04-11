abc0d452a78b249763c7abeab4f3a8da7c89afbf
TyDieGuy99 <120081348+TyDieGuy99@users.noreply.github.com>
2025-04-11 15:52:10 -0400

    cleaned up file cleraed file of console.logs and rearranged bits of code

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-console.log("Rock Paper Scissors!");
-const buttons = document.querySelectorAll('button');
+//variables
 const max = 3; //all 3 rps options
 
 let humanScore = 0;
 let computerScore = 0;
 
+const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         console.log(button.id);
@@ -14,157 +14,163 @@ buttons.forEach((button) => {
     });
 });
 
+//parent elements
 const info = document.querySelector('#info');
+const score = document.querySelector('#score');
 
+//content elements to be changed when needed
 const pChoice = document.createElement('p');
 pChoice.classList.add('pChoice');
 pChoice.textContent = 'Player: NONE';
+pChoice.style.paddingRight = '15px'
 info.appendChild(pChoice);
 
 const cChoice = document.createElement('p');
 cChoice.classList.add('cChoice');
-cChoice.textContent = 'CPU: NONE';
+cChoice.textContent = 'CPU: NONE'
+cChoice.style.paddingLeft = '15px'
 info.appendChild(cChoice);
 
 const totalScore = document.createElement('p');
 totalScore.classList.add('totalScore');
-totalScore.textContent = humanScore + '-' + computerScore;
-info.appendChild(totalScore);
+totalScore.textContent = humanScore + ' : ' + computerScore;
+totalScore.style.fontSize = '32px';
+score.appendChild(totalScore);
 
 const roundResult = document.createElement('p');
 roundResult.classList.add('roundResult');
 roundResult.textContent = '';
-info.appendChild(roundResult);
+score.appendChild(roundResult);
 
 const winner = document.createElement('p');
 winner.classList.add('winner');
 winner.textContent = '';
-info.appendChild(winner);
-
-
-
-//run it
-//playRound(humanSelection, computerSelection);
+winner.style.fontSize = '64px';
+score.appendChild(winner);
 
 //functions go down here
 
 //this converts random number 0-2 into rps option for cpu
 function getComputerChoice() {
-
     let x = Math.floor(Math.random() * max);
     let computerChoice = 'none';
 
     if (x == 0) { 
         computerChoice = 'rock';
-        console.log('com rock');
         cChoice.textContent = 'CPU: ROCK';
         return computerChoice;
     } else if (x == 1) {
         computerChoice = 'paper';
-        console.log('com paper');
         cChoice.textContent = 'CPU: PAPER';
         return computerChoice;
     } else { 
         computerChoice = 'scissors';
-        console.log('com scissors');
         cChoice.textContent = 'CPU: SCISSORS';
         return computerChoice;
     }
-    
 }
 
 //use prompt to get player input
 function getHumanChoice(buttonChoice) {
     let playerChoice = buttonChoice;
+
     if (playerChoice.toLowerCase() === 'rock') {
-        console.log('player rock');
         pChoice.textContent = 'Player: ROCK';
         return playerChoice;
     } else if (playerChoice.toLowerCase() === 'paper') {
-        console.log('player paper');
         pChoice.textContent = 'Player: PAPER';
         return playerChoice;
     } else if (playerChoice.toLowerCase() === 'scissors') {
-        console.log('player scissors');
         pChoice.textContent = 'Player: SCISSORS';
         return playerChoice;
-    } else {
-        return null //if player doesn't type any of the 3 options
     }
      
 }
 
 //play rps round
 function playRound(humanChoice, computerChoice) {
-    console.log(humanChoice);
-    console.log(computerChoice);
     //if player picks rock
      
     if (humanChoice == 'rock' && computerChoice == 'scissors') { //win condition
         humanScore++;
-        totalScore.textContent = humanScore + '-' + computerScore;
+        totalScore.textContent = humanScore + ' : ' + computerScore;
         roundResult.textContent = 'Rock beats Scissors! You Win!!!';
-        console.log("Rock beats Scissors! You Win!!!");
-        checkWinner();
 
     } else if (humanChoice == 'rock' && computerChoice == 'paper') { //lose condition
         computerScore++;
-        totalScore.textContent = humanScore + '-' + computerScore;
+        totalScore.textContent = humanScore + ' : ' + computerScore;
         roundResult.textContent = 'Paper beats Rock! You Lose!!!';
-        console.log("Paper beats Rock! You Lose!!!");
-        checkWinner();
+
         //if player picks paper
         
     } else if (humanChoice == 'paper' && computerChoice == 'scissors') { //lose condition
         computerScore++;
-        totalScore.textContent = humanScore + '-' + computerScore;
+        totalScore.textContent = humanScore + ' : ' + computerScore;
         roundResult.textContent = 'Scissors beats Paper! You Lose!!!';
-        console.log("Scissors beats Paper! You Lose!!!");
-        checkWinner();
         
     } else if (humanChoice == 'paper' && computerChoice == 'rock') { //win condition
         humanScore++;
-        totalScore.textContent = humanScore + '-' + computerScore;
+        totalScore.textContent = humanScore + ' : ' + computerScore;
         roundResult.textContent = 'Paper beats Rock! You Win!!!';
-        console.log("Paper beats Rock! You Win!!!");
-        checkWinner();
+
         //if player picks scissors
 
     } else if (humanChoice == 'scissors' && computerChoice == 'rock') { //lose condition
         computerScore++;
-        totalScore.textContent = humanScore + '-' + computerScore;
+        totalScore.textContent = humanScore + ' : ' + computerScore;
         roundResult.textContent = 'Rock beats Scissors! you Lose!!!';
-        console.log("Rock beats Scissors! you Lose!!!");
-        checkWinner();
 
     } else if (humanChoice == 'scissors' && computerChoice == 'paper') { //win condition
         humanScore++;
-        totalScore.textContent = humanScore + '-' + computerScore;
+        totalScore.textContent = humanScore + ' : ' + computerScore;
         roundResult.textContent = 'Scissors beats Paper! You Win!!!';
-        console.log("Scissors beats Paper! You Win!!!");
-        checkWinner();
+        
 
     } else { //player and cpu pick same
         roundResult.textContent = 'Draw!';
-        console.log("Draw!");
     }
-
-    console.log(humanScore);
-    console.log(computerScore);
+    
+        checkWinner();
 }
 
 //play full game of rps
 function checkWinner() {
+    
     if ((humanScore == 5) || (computerScore == 5)) {
         if (humanScore > computerScore) {
-            console.log("You Win!! Score: " + humanScore + ":" + computerScore)
-            winner.textContent = 'You won the game!';
+            if (computerScore != 0) {
+                winner.textContent = 'You won the game!';
+            } else {
+                winner.textContent = 'You\'re a rock paper scissors masters!!!';
+            }
+            
         } else if (humanScore < computerScore) {
-            console.log("You Lose!! Score: " + humanScore + ":" + computerScore)
-            winner.textContent = 'You lost the game!';
-        } else {
-            console.log("The match is a draw!")
+            if (humanScore != 0 ) {
+                winner.textContent = 'You lost the game!';
+            } else {
+                winner.textContent = 'Yikes!!...'
+            }
+            
         }
+        //prompt player if they wish to play again - if yes reset everything to 0, if no disable buttons
+        setTimeout(function() { //delay the confirm to allow brower to update results properly
+            if (confirm('Play again??')) {
+                console.log('you want to play again');
+                humanScore = 0;
+                computerScore = 0;
+                roundResult.textContent = '';
+                winner.textContent = '';
+                cChoice.textContent = 'CPU: NONE';
+                pChoice.textContent = 'Player: NONE';
+                totalScore.textContent = humanScore + ' : ' + computerScore;
+            } else {
+                console.log('you do not want to play again');
+                buttons.forEach((button) => {
+                    button.disabled = true;
+                });
+            }
+        }, 1);
+        
     }
+    
 }
-
